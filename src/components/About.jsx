fc82bce1e0dac86562b1024f5dae94d024fc165d
Dany Oblitas <68875920+ddanielos@users.noreply.github.com>
2024-02-12 04:05:13 +0000

    margin about ok

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,7 +12,7 @@ const About = () => {
       </div>
       <div className="flex flex-col md:flex-row md:h-[550px]">
         <div className=" text-primary-color bg-second-color w-full h-300 md:h-auto md:w-[50vw] flex flex-row content-evenly">
-          <section className="text-center pt-12 pb-24 px-4 md:px-auto md:pb-auto m-auto">
+          <section className="text-center pt-12 pb-24 px-4 md:px-auto md:pb-auto m-auto md:mx-[90px] md:mt-[20px]">
             <h2 className="uppercase font-bioRhyme text-xl md:text-4xl font-extrabold tracking-[0.2rem] pb-3">Quienes Somos</h2>
             <p className='font-roboto text-sm md:text-xl tracking-[0.1rem] leading-7 pt-[2rem] text-justify'>Somos una cervecería artesanal desde 2014 localizada en el corazón del Valle Sagrado de los inKas, en el pueblo de Yucay.</p>
             <p className='font-roboto text-sm md:text-xl tracking-[0.1rem] leading-7 pt-[2rem] text-justify'>Creemos que las cosas buenas de la vida requieren valentia para lograr la verdadera independencia haciendo algo que se ama.</p>
@@ -20,7 +20,7 @@ const About = () => {
           </section>
         </div>
         <div className="text-primary-color bg-fourth-color w-full md:w-[50vw] h-96 md:h-auto flex flex-row content-evenly">
-          <section className="text-center pt-24 md:pt-12 pb-12 px-4 md:px-auto md:pb-auto m-auto">
+          <section className="text-center pt-24 md:pt-12 pb-12 px-4 md:px-auto md:pb-auto m-auto md:mx-[90px]  md:mt-[20px]">
             <h2 className='uppercase font-bioRhyme text-xl md:text-4xl font-extrabold tracking-[0.2rem] pb-3'>Que queremos</h2>
             <p className='font-roboto text-sm md:text-xl tracking-[0.1rem] leading-7 pt-[2rem] text-justify'>Queremos inspirar.</p>
             <p className='font-roboto text-sm md:text-xl tracking-[0.1rem] leading-7 pt-[2rem] text-justify'>Queremos generar una comunidad en torno a lo que más felices nos hace. Nuestra tierra, nuestra cultura y el proceso cervecero.</p>
