ac06bff311f4284bf8c058989e2bb35e0bd2cf5d
Dany Oblitas <68875920+ddanielos@users.noreply.github.com>
2024-02-14 15:33:54 +0000

    update carousel mobile - blogitem width adjustment

--- src/components/BlogItem.jsx
+++ src/components/BlogItem.jsx
@@ -9,7 +9,7 @@ export default function BlogItem(props){
   return(
     <div className="py-[20px] md:py-[60px] md:px-0">
       <Link className="visited:text-inherit visited:no-underline visited:outline-none" href={`/blogs/${Slug}`}>
-        <div className=" rounded-2xl overflow-hidden shadow-[6px_6px_3px_rgba(198,106,72,0.3)] hover:-translate-y-[2%] flex flex-col w-[300px] md:w-[400px] h-[450px] mr-2.5 cursor-pointer transition-all">
+        <div className=" rounded-2xl overflow-hidden shadow-[6px_6px_3px_rgba(198,106,72,0.3)] hover:-translate-y-[2%] flex flex-col w-[76vw] md:w-[400px] h-[450px] mr-2.5 cursor-pointer transition-all">
           <div className=" w-[400px] h-[250px] overflow-hidden relative"><img className=" w-full h-auto" src={cover_img}/></div>
           <div className="h-[200px] flex bg-primary-color flex-col content-evenly p-[20px] text-justify leading-5 transition-opacity duration-500">
             <h3 className=" font-roboto uppercase text-base my-[10px] font-bold">
