31d85571ad5802ac1ec28210d820fd1f34e781fa
Dany Oblitas <68875920+ddanielos@users.noreply.github.com>
2025-11-19 03:56:29 +0000

    update jhom v2.1

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
         <div className="">
           <div className="bg-hero-image bg-cover bg-center flex flex-col justify-around items-center h-80 md:h-300" >
               <Image className=" w-60" src="/assets/hero.png" priority="high" alt="cervecería artesanal" width={334} height={272}/>
-              <h2 className="text-primary-color uppercase font-roboto font-bold text-sm md:text-lg xl:text-xl [word-spacing: 1.4em]">"Tradición andina, alma cervecera"</h2>
+              <h2 className="text-primary-color uppercase font-roboto font-bold text-sm md:text-lg xl:text-xl [word-spacing: 1.4em]">Tradición andina, alma cervecera</h2>
           </div>
         </div>
       </>
